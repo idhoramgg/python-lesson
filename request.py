55b465f629da7402679a9940f4d127fdb5dfb1ad
Ridho Abdul Majid <ridhoabdulmajid@gmail.com>
2020-07-10 20:03:49 +0700

    cobain

---
+++ request.py
@@ -0,0 +1,11 @@
+#import library / dependecy
+import requests
+
+# get request to google.com
+try:
+    r = requests.get('https://google.com')
+    print(r.status_code)
+    if r.status_code == 200:
+        print(r.text)
+except Exception as e:
+    print('ada error', e)
